3f38cf7efcbfefb59f37984f324aeeb72a26f00f
Hyeonii <ftoyl_me@naver.com>
2024-04-23 20:55:11 +0900

    [feat] Active link menu

--- bird-nest/src/app/(has-menu)/_components/Menu.tsx
+++ bird-nest/src/app/(has-menu)/_components/Menu.tsx
@@ -1,37 +1,57 @@
+'use client'
 import Link from 'next/link'
 import Logo from '../../../../public/logo.svg'
+import { useSelectedLayoutSegment } from 'next/navigation'
 
 export default function Menu() {
+  const segment = useSelectedLayoutSegment()
   const menus = [
     {
       title: '홈',
       url: '/',
+      isPrivate: false,
     },
     {
       title: '검색',
-      url: '/explore',
+      url: 'explore',
+      isPrivate: false,
     },
     {
-      title: '내정보',
-      url: '/mypage',
+      title: '내 정보',
+      url: 'mypage',
+      isPrivate: true,
     },
   ]
+  const me = {
+    id: 'hyeonii',
+  }
 
   return (
     <div className="w-min-menu">
       <Logo className="mb-2 mt-3" width={40} height={40} alt="Logo" />
-      <ul>
+      <nav>
         {menus.map((menu, idx) => {
+          let isSelected = false
+          let isLogedIn = !!me
+
+          if (!segment && menu.url === '/') isSelected = true
+          if (segment === menu.url) isSelected = true
+
+          if (menu.isPrivate && !isLogedIn) return <></>
+
           return (
-            <li key={menu.title + idx} className="p-2 font-bold">
+            <li
+              key={menu.title + idx}
+              className={`ml-2 mt-4 ${isSelected ? 'text-xl font-extrabold' : 'font-bold'} hover:text-neutral-500`}
+            >
               <Link href={menu.url}>{menu.title}</Link>
             </li>
           )
         })}
-        <li className="m-2 h-10 w-10 rounded-full bg-orange-400 text-center text-3xl text-white">
-          <Link href="new-post">+</Link>
+        <li className="ml-2 mt-4 w-20 rounded-xl bg-orange-400 text-center font-bold text-white hover:bg-orange-300">
+          <Link href="new-post">+ 글 작성</Link>
         </li>
-      </ul>
+      </nav>
     </div>
   )
 }
